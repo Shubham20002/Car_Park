cb9a1c44f1bd8a5f093a211d3b821c284811d278
shubham <shubham992284@gmail.com>
2023-07-13 13:55:23 +0530

    atlas aonnected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,14 @@
 // getting-started.js
 const mongoose = require('mongoose');
-
+const atlas=`mongodb+srv://shubham992284:Sanju1967%40dp@cluster0.y4x6xrw.mongodb.net/node_authenticate`;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Node_Authentication');
+  await mongoose.connect(atlas);
   console.log("connected to db");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+
+
